d76414c7c25d2aa4ddc68be6cfa0353de52fe781
haesung <ellen1216@naver.com>
2022-11-15 14:56:10 +0900

    event 검색창으로 연결되도록 수정 + 상단바 초록색 포커싱

--- Community_Result_Page.php
+++ Community_Result_Page.php
@@ -10,7 +10,7 @@
     <div class="div_mainbar">
         <div class="div_logo">
         <hr class="hr_logo">
-        <h1 class="h1_logo">WHO's TT</h1>
+        <h1 class="h1_logo" onclick="location.href='Main_Page.php'">WHO's TT</h1>
         <hr class="hr_logo">
         </div>
         <div class="div_category">
@@ -19,8 +19,8 @@
                 <li><a onclick="location.href='Season_Page.php'">SEASONS</a></li>
                 <li><a onclick="location.href='Favor_Page.php'">OTT SERVICE</a></li>
                 <li><a onclick="location.href='Genre_Page.php'">GENRE</a></li>
-                <li><a class="text_green" onclick="location.href='Event_Page_autoscroll.php'">EVENT</a></li>
-                <li><a onclick="location.href='Community_Page.php'">COMMUNITY</a></li>
+                <li><a onclick="location.href='Event_Page_autoscroll.php'">EVENT</a></li>
+                <li><a class="text_green" onclick="location.href='Community_Result_Page.php'">COMMUNITY</a></li>
                 <li><a onclick="location.href='Actor_Page.php'">KOREAN ACTOR</a></li>
             </ul>
         </nav>
